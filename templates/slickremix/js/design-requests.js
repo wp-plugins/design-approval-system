5453527cea2117ff517508eba02ddcde96db38cf
slickremix <slickremix@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-13 21:14:09 +0000

    git-svn-id: https://plugins.svn.wordpress.org/design-approval-system/trunk@585066 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/slickremix/js/design-requests.js
+++ templates/slickremix/js/design-requests.js
@@ -1,48 +1,48 @@
-$(document).ready(function() {	
+jQuery(document).ready(function() {	
 //Pop-ups
-	$('.design-approval-btn').click(function () {	
-		$('.pop-up-backg, .approved-form-wrap').fadeIn();
+	jQuery('.design-approval-btn').click(function () {	
+		jQuery('.pop-up-backg, .approved-form-wrap').fadeIn();
 	});
 	
-	$('#paid').click(function () {	
-		$('.pop-up-backg, .design-request-form-wrap').fadeIn();
+	jQuery('#paid').click(function () {	
+		jQuery('.pop-up-backg, .design-request-form-wrap').fadeIn();
 	});
 	
-	$('#not-paid').click(function () {	
-		$('.pop-up-backg, .additional-design-request-form-wrap').fadeIn();
+	jQuery('#not-paid').click(function () {	
+		jQuery('.pop-up-backg, .additional-design-request-form-wrap').fadeIn();
 	});
-	$('.close, #cancel').click(function () {	
-		$('.pop-up-backg, .approved-form-wrap, .design-request-form-wrap, .additional-design-request-form-wrap').fadeOut();
+	jQuery('.close, #cancel').click(function () {	
+		jQuery('.pop-up-backg, .approved-form-wrap, .design-request-form-wrap, .additional-design-request-form-wrap').fadeOut();
 	});
 	
 //Tabs
-$('.comments-tab').addClass('design-tab-selected');
+jQuery('.comments-tab').addClass('design-tab-selected');
 	
-	$('.comments-tab').click(function () {	
-		$('.comments-tab').addClass('design-tab-selected');
-		$('.changes-tab, .additions-tab, .omissions-tab').removeClass('design-tab-selected');
-		$('#b2, #b3, #b4').hide();
-		$('#b1').slideDown();
+	jQuery('.comments-tab').click(function () {	
+		jQuery('.comments-tab').addClass('design-tab-selected');
+		jQuery('.changes-tab, .additions-tab, .omissions-tab').removeClass('design-tab-selected');
+		jQuery('#b2, #b3, #b4').hide();
+		jQuery('#b1').slideDown();
 	});
 
-	$('.changes-tab').click(function () {	
-		$('.changes-tab').addClass('design-tab-selected');
-		$('.comments-tab, .additions-tab, .omissions-tab').removeClass('design-tab-selected');
-		$('#b1, #b3, #b4').hide();
-		$('#b2').slideDown();
+	jQuery('.changes-tab').click(function () {	
+		jQuery('.changes-tab').addClass('design-tab-selected');
+		jQuery('.comments-tab, .additions-tab, .omissions-tab').removeClass('design-tab-selected');
+		jQuery('#b1, #b3, #b4').hide();
+		jQuery('#b2').slideDown();
 	});
 	
-	$('.additions-tab').click(function () {	
-		$('.additions-tab').addClass('design-tab-selected');
-		$('.comments-tab, .changes-tab, .omissions-tab').removeClass('design-tab-selected');
-		$('#b1, #b2, #b4').hide();
-		$('#b3').slideDown();
+	jQuery('.additions-tab').click(function () {	
+		jQuery('.additions-tab').addClass('design-tab-selected');
+		jQuery('.comments-tab, .changes-tab, .omissions-tab').removeClass('design-tab-selected');
+		jQuery('#b1, #b2, #b4').hide();
+		jQuery('#b3').slideDown();
 	});
 
-	$('.omissions-tab').click(function () {	
-		$('.omissions-tab').addClass('design-tab-selected');
-		$('.comments-tab, .changes-tab, .additions-tab').removeClass('design-tab-selected');
-		$('#b1, #b2, #b3').hide();
-		$('#b4').slideDown();
+	jQuery('.omissions-tab').click(function () {	
+		jQuery('.omissions-tab').addClass('design-tab-selected');
+		jQuery('.comments-tab, .changes-tab, .additions-tab').removeClass('design-tab-selected');
+		jQuery('#b1, #b2, #b3').hide();
+		jQuery('#b4').slideDown();
 	});
 });	
