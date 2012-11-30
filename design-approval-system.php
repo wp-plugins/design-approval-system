2d62a9deb9094ebd2a00d8cc82b05649e23412b1
slickremix <slickremix@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-30 02:00:28 +0000

    betterwork
    git-svn-id: https://plugins.svn.wordpress.org/design-approval-system/trunk@632069 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- design-approval-system.php
+++ design-approval-system.php
@@ -3,12 +3,21 @@
 Plugin Name: Design Approval System
 Plugin URI: http://slickremix.com/
 Description: A plugin to display Designs and have a client approve them by giving a digital signature.
+<<<<<<< .mine
+Version: 2.3
+Author: Slick Remix [Justin Labadie & Spencer Labadie]
+=======
 Version: 2.2
 Author: Slick Remix [Justin Labadie & Spencer Labadie]
+>>>>>>> .r632067
 Author URI: http://slickremix.com/
 Requires at least: wordpress 3.4.0
 Tested up to: wordpress 3.5.0
+<<<<<<< .mine
+Stable tag: 2.3
+=======
 Stable tag: 2.2
+>>>>>>> .r632067
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -29,4 +38,17 @@ include( 'includes/das-functions.php' );
 include( 'includes/das-meta-box.php' );
 include( 'admin/das-settings-page.php' );
 include( 'admin/das-help-page.php' );
+include( 'admin/das-video-page.php' );
+include( 'admin/das-news-updates-page.php' );
+
+/**
+ * Returns current plugin version. SRL added
+ * 
+ * @return string Plugin version
+ */
+function dasystem_version() {
+	$plugin_data = get_plugin_data( __FILE__ );
+	$plugin_version = $plugin_data['Version'];
+	return $plugin_version;
+}
 ?>
